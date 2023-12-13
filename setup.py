33485f36232a3f76fdb616c6ae74a80ee217a408
David Manthey <david.manthey@kitware.com>
2023-12-13 14:38:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     include_package_data=True,
     keywords='girder, fuse, girder-client',
     packages=find_packages(exclude=['test', 'test.*']),
-    url='https://github.com/manthey/girder-client-mount',
+    url='https://github.com/DigitalSlideArchive/girder-client-mount',
     zip_safe=False,
     python_requires='>=3.8',
     entry_points={
